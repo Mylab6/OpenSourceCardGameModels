72fe4d1f48127e1ea27d0f0eb542351fee0fa11c
mylab6 <dreamihad@gmail.com>
2024-12-28 13:32:14 -0600

    a

--- lib/flutter_card_game_models.dart
+++ lib/flutter_card_game_models.dart
@@ -1,11 +1,11 @@
 /// Support for doing something awesome.
 ///
 /// More dartdocs go here.
-library;
+library flutter_card_game_models;
 
 export 'src/flutter_card_game_models_base.dart';
-export 'src/models/card_effect.dart';
-export 'src/models/card_type.dart';
-export 'src/models/game_card.dart';
+export 'card_effect.dart';
+export 'card_type.dart';
+export 'game_card.dart';
 
 // TODO: Export any libraries intended for clients of this package.
